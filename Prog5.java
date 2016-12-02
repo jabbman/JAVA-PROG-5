d84464a0c551a212627bb9bec9c6e06f254611cf
jabbman <qzhs@iup.edu>
2016-12-02 00:41:15 -0500

    Add files via upload

---
+++ Prog5.java
@@ -0,0 +1,18 @@
+/**
+ * COSC 310		Assignment 5
+ * Prog5.java
+ * 3/26/15
+ * A client class that Simulates time of arrival and number of customers waiting
+ * based off of psuedo-random number .
+ 
+ */
+public class Prog5 { // client program
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Simulation sim = new Simulation (100);
+		sim.run();
+	}
+
+}
